8942de5db655b03cb2d1c7e7487ff919931aebb7
SindreEieLedsaak <sindre.eie@gmail.com>
2025-09-08 12:29:23 +0200

    Default secret key for testing

--- backend/services/auth_service.py
+++ backend/services/auth_service.py
@@ -21,7 +21,7 @@ oauth.register(
 )
 
 # --- JWT Configuration ---
-SECRET_KEY = os.getenv("SECRET_KEY")
+SECRET_KEY = os.getenv("SECRET_KEY", "a_very_secret_key_that_should_be_in_env")
 ALGORITHM = "HS256"
 ACCESS_TOKEN_EXPIRE_MINUTES = 30
 
